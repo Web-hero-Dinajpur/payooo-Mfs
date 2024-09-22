30608c4b7add7de612c274ee91e1d7d851e206b7
Sreedhar-roy <sreedharroy0@gmail.com>
2024-09-23 00:02:07 +0600

    transsection section done

--- final-project/js/featurs.js
+++ final-project/js/featurs.js
@@ -1,1 +1,12 @@
-console.log('bangladeash')
+document.getElementById('show-add-money-form').addEventListener('click', function(){
+    // console.log('show add money button click')
+    getShowHistoryById('Add-money-section');
+
+})
+document.getElementById('show-cash-out-form').addEventListener('click', function(){
+    getShowHistoryById('cash-Out-section');
+})
+
+document.getElementById('show-transfar-form').addEventListener('click', function(){
+    getShowHistoryById("transaction-section");
+})
--- final-project/js/ulilize.js
+++ final-project/js/ulilize.js
@@ -9,3 +9,14 @@ function getInputFieldTextById(id) {
     const NewBalance = parseFloat(addNewText);
     return NewBalance;
 }
+
+function getShowHistoryById(id){
+    //hide all the section
+    document.getElementById('Add-money-section').classList.add('hidden');
+    document.getElementById('cash-Out-section').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+
+    //show the section with the provide id as parameter
+
+    document.getElementById(id).classList.remove('hidden');
+}
